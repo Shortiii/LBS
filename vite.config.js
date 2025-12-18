7f899ace0609fe4373fa05a6d8376c538b271728
Oluwatoyin <105533960+Shortiii@users.noreply.github.com>
2025-12-18 09:58:25 +0100

    fixing the page bug

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,25 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        alumni: resolve(__dirname, 'alumni.html'),
+        career: resolve(__dirname, 'career.html'),
+        // news: resolve(__dirname, 'news.html'),
+        login: resolve(__dirname, 'login.html'),
+        event: resolve(__dirname, 'event.html'),
+        emptyEvent: resolve(__dirname, 'emptyEvent.html'),
+        moreEvent: resolve(__dirname, 'moreEvent.html'),
+        resource: resolve(__dirname, 'resource.html'),
+        resourceAudio: resolve(__dirname, 'resourceAudio.html'),
+        resourceVideo: resolve(__dirname, 'resourceVideo.html'),
+        resourceBook: resolve(__dirname, 'resourceBook.html'),
+      }
+    }
+  }
 })
